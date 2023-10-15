683f56e44ffffa339c5874105043218adbbe0687
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-10-15 13:53:44 -0600

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-// import { resolve } from 'path';
 import { defineConfig } from "vite";
 
 export default defineConfig({
   root: "src/",
   build: {
-    rollupOptions: {
-      input: "src/index.html",
-    },
   },
 });
