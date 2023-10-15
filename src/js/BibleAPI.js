0d7bd8e70079765a7fe785ca562ad167f367f0ac
ReArquitecto <94197258+ReArquitecto@users.noreply.github.com>
2023-10-14 18:43:13 -0600

    loads of changes

--- src/js/BibleAPI.js
+++ src/js/BibleAPI.js
@@ -1,86 +1,259 @@
 export default class BibleAPI {
-  constructor() {
-      this.API_KEY = 'd42753bd397e6f90fcaaa710a6dbfbdf';
+  constructor(apiKey) {
+    this.API_KEY = apiKey;
+    this.BASE_URL = "https://api.scripture.api.bible/v1/";
+    this.TIMEOUT = 5000; // Set a timeout of 5 seconds (adjust as needed).
   }
 
-  async makeRequest(url) {
-      return new Promise((resolve, reject) => {
-          const xhr = new XMLHttpRequest();
-          xhr.withCredentials = false;
+  async makeRequest(endpoint) {
+    const url = new URL(endpoint, this.BASE_URL);
 
-          xhr.onreadystatechange = function() {
-              if (this.readyState === this.DONE) {
-                  if (this.status === 200) {
-                      resolve(JSON.parse(this.responseText).data);
-                  } else {
-                      reject(new Error(this.statusText));
-                  }
-              }
-          };
+    try {
+      const response = await Promise.race([
+        fetch(url.toString(), {
+          headers: {
+            "api-key": this.API_KEY,
+          },
+        }),
+        new Promise((_, reject) =>
+          setTimeout(() => reject(new Error("Request timeout")), this.TIMEOUT)
+        ),
+      ]);
 
-          xhr.open('GET', url);
-          xhr.setRequestHeader('api-key', this.API_KEY);
+      if (!response.ok) {
+        throw new Error(`Request failed with status ${response.status}`);
+      }
 
-          xhr.onerror = () => reject(new Error(xhr.statusText));
-
-          xhr.send();
-      });
+      return response.json();
+    } catch (error) {
+      console.error("Error making request:", error);
+      throw error;
+    }
   }
 
   async getBooks(bibleVersionID) {
-      const url = `https://v2.api.bible/bibles/${bibleVersionID}/books`;
-      const data = await this.makeRequest(url);
-      return data.map(({ name, id }) => ({ name, id }));
+    const endpoint = `/bibles/${bibleVersionID}/books`;
+    const data = await this.makeRequest(endpoint);
+    return data.map(({ name, id }) => ({ name, id }));
   }
 
   async getChapters(bibleVersionID, bibleBookID) {
-      const url = `https://v2.api.bible/bibles/${bibleVersionID}/books/${bibleBookID}/chapters`;
-      const data = await this.makeRequest(url);
-      return data.map(({ number, id }) => ({ number, id }));
+    const endpoint = `/bibles/${bibleVersionID}/books/${bibleBookID}/chapters`;
+    const data = await this.makeRequest(endpoint);
+    return data.map(({ number, id }) => ({ number, id }));
   }
 
   async getVerses(bibleVersionID, bibleChapterID) {
-      const url = `https://v2.api.bible/bibles/${bibleVersionID}/chapters/${bibleChapterID}/verses`;
-      const data = await this.makeRequest(url);
-      return data.map(({ id }) => ({ id }));
+    const endpoint = `/bibles/${bibleVersionID}/chapters/${bibleChapterID}/verses`;
+    const data = await this.makeRequest(endpoint);
+    return data.map(({ id }) => ({ id }));
   }
 
   async getBibleVersions() {
-      const url = 'https://v2.api.bible/bibles';
-      const data = await this.makeRequest(url);
-      return data.map(({ name, id, abbreviation, description, language }) => ({
-          name, 
-          id, 
-          abbreviation, 
-          description, 
-          language: language.name
-      }));
+    const endpoint = "/bibles";
+    const data = await this.makeRequest(endpoint);
+    return data.map(({ name, id, abbreviation, description, language }) => ({
+      name,
+      id,
+      abbreviation,
+      description,
+      language: language.name,
+    }));
   }
 
-  async getAllData(bibleVersionID) {
-      const finalData = {};
+  async LoadBibleData(bibleVersionID) {
+    const finalData = {};
 
-      try {
-          const books = await this.getBooks(bibleVersionID);
-          
-          for (let book of books) {
-              const bookName = book.name;
-              finalData[bookName] = {};
+    try {
+      const books = await this.getBooks(bibleVersionID);
 
-              const chapters = await this.getChapters(bibleVersionID, book.id);
-              
-              for (let chapter of chapters) {
-                  const chapterNumber = chapter.number;
+      for (let book of books) {
+        const bookName = book.name;
+        finalData[bookName] = {};
 
-                  const verses = await this.getVerses(bibleVersionID, chapter.id);
-                  finalData[bookName][chapterNumber] = verses.map(verse => verse.id);
-              }
-          }
+        const chapters = await this.getChapters(bibleVersionID, book.id);
 
-          return finalData;
-      } catch (error) {
-          console.error("Error fetching Bible data:", error);
-          throw error;
+        for (let chapter of chapters) {
+          const chapterNumber = chapter.number;
+
+          const verses = await this.getVerses(bibleVersionID, chapter.id);
+          finalData[bookName][chapterNumber] = verses.map((verse) => verse.id);
+        }
       }
+
+      return finalData;
+    } catch (error) {
+      console.error("Error fetching Bible data:", error);
+      return {}; // Return an empty object or handle the error as needed.
+    }
+  }
+
+  LoadMockData() {
+    return {
+      oldtestament: {
+        genesis: {
+          1: 31,
+          2: 25,
+          3: 24,
+          4: 26,
+          5: 32,
+          6: 22,
+          7: 24,
+        },
+        exodus: {
+          1: 22,
+          2: 25,
+          3: 22,
+          4: 31,
+          5: 23,
+          6: 30,
+          7: 25,
+        },
+        leviticus: {
+          1: 17,
+          2: 16,
+          3: 17,
+          4: 35,
+          5: 19,
+          6: 30,
+          7: 38,
+        },
+        numbers: {
+          1: 54,
+          2: 34,
+          3: 51,
+          4: 49,
+          5: 31,
+          6: 27,
+          7: 89,
+        },
+        deuteronomy: {
+          1: 46,
+          2: 37,
+          3: 29,
+          4: 49,
+          5: 33,
+          6: 25,
+          7: 26,
+        },
+        joshua: {
+          1: 18,
+          2: 24,
+          3: 17,
+          4: 24,
+          5: 15,
+          6: 27,
+          7: 26,
+        },
+        judges: {
+          1: 36,
+          2: 23,
+          3: 31,
+          4: 24,
+          5: 31,
+          6: 40,
+          7: 25,
+        },
+        ruth: {
+          1: 22,
+          2: 23,
+          3: 18,
+          4: 22,
+        },
+        "1 samuel": {
+          1: 28,
+          2: 36,
+          3: 21,
+          4: 22,
+          5: 12,
+          6: 21,
+          7: 17,
+        },
+        "2 samuel": {
+          1: 27,
+          2: 32,
+          3: 39,
+          4: 12,
+          5: 25,
+          6: 23,
+          7: 29,
+        },
+        "1 kings": {
+          1: 53,
+          2: 46,
+          3: 28,
+          4: 34,
+          5: 18,
+          6: 38,
+          7: 51,
+        },
+        "2 kings": {
+          1: 18,
+          2: 25,
+          3: 27,
+          4: 44,
+          5: 27,
+          6: 33,
+          7: 20,
+        },
+      },
+      newtestament: {
+        matthew: {
+          1: 25,
+          2: 23,
+          3: 17,
+          4: 25,
+          5: 48,
+          6: 34,
+          7: 29,
+        },
+        mark: {
+          1: 45,
+          2: 28,
+          3: 35,
+          4: 41,
+          5: 43,
+          6: 56,
+          7: 37,
+        },
+        luke: {
+          1: 80,
+          2: 52,
+          3: 38,
+          4: 44,
+          5: 39,
+          6: 49,
+          7: 50,
+        },
+        john: {
+          1: 51,
+          2: 25,
+          3: 36,
+          4: 54,
+          5: 47,
+          6: 71,
+          7: 53,
+        },
+        acts: {
+          1: 26,
+          2: 47,
+          3: 26,
+          4: 37,
+          5: 42,
+          6: 15,
+          7: 60,
+        },
+        romans: {
+          1: 32,
+          2: 29,
+          3: 31,
+          4: 25,
+          5: 21,
+          6: 23,
+          7: 25,
+        },
+      },
+      translations: ["NIV", "KJV", "ESV", "NLT", "MSG"],
+    };
   }
 }
--- src/js/navigator.js
+++ src/js/navigator.js
@@ -1,58 +1,55 @@
-import BibleAPI from './BibleAPI.js';
+import BibleAPI from "./BibleAPI.js";
 
 class BibleNavigator {
   constructor() {
+    this.apiKey = "d42753bd397e6f90fcaaa710a6dbfbdf";
 
-    this.appContent = document.getElementById('app-content');
-    this.breadcrumbElement = document.getElementById('breadcrumb');
 
+    // switch between mock data and api data
+    this.BibleAPI = new BibleAPI(this.apiKey);
+    this.bibleData = this.BibleAPI.LoadMockData();
+    // this.bibleData = this.LoadBibleData(this.apiKey);
+    console.log(JSON.stringify(this.bibleData, null, 2));    
+
+    this.appContent = document.getElementById("app-content");
+    this.breadcrumbElement = document.getElementById("breadcrumb");
     this.bindNavigation();
-    this.LoadBibleData();
-
-
-    // Mock data for testing
-    this.otbooks = ['Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy', 'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel', '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles', 'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalms', 'Proverbs', 'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah', 'Lamentations', 'Ezekiel', 'Daniel', 'Hosea', 'Joel', 'Amos'];
-    this.ntbooks = ['Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1 Corinthians', '2 Corinthians', 'Galatians', 'Ephesians', 'Philippians', 'Colossians'];
-    this.chapters = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-    this.verses = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
-    this.translations = ["KJV", "NIV", "ESV"];
 
     this.bindEvents();
-    this.navigate();
+    this.navigateByURL();
   }
 
   bindNavigation() {
-    window.addEventListener('popstate', () => this.navigate());
-
+    window.addEventListener("popstate", () => this.navigateByURL());
   }
 
   async LoadBibleData() {
     try {
-        const api = new BibleAPI();
-        const bibleVersionID = 'de4e12af7f28f599-02';
+      const api = new BibleAPI(apiKey);
+      const bibleVersionID = "de4e12af7f28f599-02";
 
-        // Call the getAllData method and get the result.
-        const data = await api.getAllData(bibleVersionID);
-        
-        console.log(JSON.stringify(data, null, 2));  // pretty print the data
+      // Call the getAllData method and get the result.
+      const data = await api.getAllData(bibleVersionID);
 
+      console.log(JSON.stringify(data, null, 2)); // pretty print the data
     } catch (error) {
-        console.error("Error:", error);
+      console.error("Error:", error);
     }
   }
 
-
-  navigate() {
-    console.log('navigating');
-    const { book, chapter, verse } = this.getURLParams();
+  navigateByURL() {
+    let { book, chapter, verse } = this.getURLParams();
+    book = book ? book.toLowerCase() : null;
+    const testament = this.getTestament(book);
     console.log(book, chapter, verse);
+    this.redirectIfInvalid();
 
     if (book && chapter && verse) {
-      this.loadVersesPage(book, chapter, verse);
+      this.loadTranslationPage(book, chapter, verse);
     } else if (book && chapter) {
-      this.loadChaptersPage(book, chapter);
+      this.loadVersesPage(book, chapter);
     } else if (book) {
-      this.loadHomePage();
+      this.loadChaptersPage(book);
     } else {
       this.loadHomePage();
     }
@@ -62,34 +59,43 @@ class BibleNavigator {
     console.log(book, chapter, verse);
     if (book && chapter && verse) {
       window.history.pushState({}, "", `/${book}/${chapter}/${verse}`);
-    }
-    else if (book && chapter) {
+    } else if (book && chapter) {
       window.history.pushState({}, "", `/${book}/${chapter}`);
-    }
-    else if (book) {
+    } else if (book) {
       window.history.pushState({}, "", `/${book}`);
-    }
-    else {
+    } else {
       window.history.pushState({}, "", `/`);
     }
   }
 
   loadHomePage() {
-    this.updateBreadcrumb()
+    this.updateBreadcrumb();
     this.updatePath();
+    this.newTestamentBooks = Object.keys(this.bibleData.newtestament);
+    this.oldTestamentBooks = Object.keys(this.bibleData.oldtestament);
     const content = `
       <div class="flex-sect">
         <div class="sect sect-grid3">
         <h2>Old Testament</h2>
         <ul>
-          ${this.otbooks.map(book => `<li><button class="book" data-book="${book}">${book}</button></li>`).join('')}
+          ${this.oldTestamentBooks
+            .map(
+              (book) =>
+                `<li><button class="book" data-book="${book}">${book}</button></li>`
+            )
+            .join("")}
         </ul>
         </div>
         
         <div class="sect sect-grid3">
         <h2>New Testament</h2>
         <ul>
-          ${this.ntbooks.map(book => `<li><button class="book" data-book="${book}">${book}</button></li>`).join('')}
+          ${this.newTestamentBooks
+            .map(
+              (book) =>
+                `<li><button class="book" data-book="${book}">${book}</button></li>`
+            )
+            .join("")}
         </ul>
         </div>
       </div>`;
@@ -97,31 +103,60 @@ class BibleNavigator {
   }
 
   loadChaptersPage(book) {
+    const testament = this.getTestament(book);
+    if (!testament) {
+      console.error(`Unable to determine testament for the book: ${book}`);
+      return;
+    }
+
+    if (!this.bibleData || !this.bibleData[testament]) {
+        console.error(`Bible data missing or does not contain the testament: ${testament}`);
+        return;
+    }
     this.updatePath(book);
     this.updateBreadcrumb();
+    this.chapters = Object.keys(this.bibleData[testament][book]);
+    console.log('chapters', this.chapters);
     const content = `
       <div class="sect sect-grid">
       <h2>${book}</h2>
       <ul>
-        ${this.chapters.map(chapter => `<li><button class="chapter" data-chapter="${chapter}">${chapter}</button></li>`).join('')}
+        ${this.chapters
+          .map(
+            (chapter) =>
+              `<li><button class="chapter" data-chapter="${chapter}">${chapter}</button></li>`
+          )
+          .join("")}
       </ul>
       </div>`;
     this.appContent.innerHTML = content;
   }
-  
-  loadVersesPage(book, chapter) {
+
+  loadVersesPage( book, chapter) {
+    const testament = this.getTestament(book);
     this.updatePath(book, chapter);
     this.updateBreadcrumb();
+
+    const totalVerses = this.bibleData[testament][book][chapter];
+    this.verses = Array.from({ length: totalVerses }, (_, i) => i + 1);
+    
+    console.log('verses', this.verses);
     const content = `
       <div class="sect sect-grid">
       <h2>${book} Chapter ${chapter}</h2>
+      
       <ul>
-        ${this.verses.map(verse => `<li><button class="verse" data-verse="${verse}">${verse}</button></li>`).join('')}
+        ${this.verses
+          .map(
+            (verse) =>
+              `<li><button class="verse" data-verse="${verse}">${verse}</button></li>`
+          )
+          .join("")}
       </ul>
       </div>`;
     this.appContent.innerHTML = content;
   }
-  
+
   loadTranslationPage(book, chapter, verse) {
     this.updatePath(book, chapter, verse);
     this.updateBreadcrumb();
@@ -129,49 +164,46 @@ class BibleNavigator {
       <div class="sect sect-translation">
       <h2>Translations for: ${book} Chapter ${chapter}:${verse}</h2>
       <ul>
-        ${this.translations.map(translation => `<li>${translation}: ${verse}</li>`).join('')}
+        ${this.bibleData.Translations
+          .map((translation) => `<li>${translation}: ${verse}</li>`)
+          .join("")}      
       </ul>
       </div>`;
     this.appContent.innerHTML = content;
   }
 
   loadPageNotFound() {
-    this.appContent.innerHTML = '<h2>Page not found</h2>';
+    this.appContent.innerHTML = "<h2>Page not found</h2>";
   }
-  
+
   updateBreadcrumb() {
     const { book, chapter, verse } = this.getURLParams();
     const parts = [book, chapter, verse].filter(Boolean);
-    parts.unshift('Home');
-    const breadcrumb = parts.map((part, index) => {
-      return `<li><a href="#" data-index="${index}">${part}</a></li>`;
-    }).join('');
+    parts.unshift("Home");
+    const breadcrumb = parts
+      .map((part, index) => {
+        return `<li><a href="#" data-index="${index}">${part}</a></li>`;
+      })
+      .join("");
     this.breadcrumbElement.innerHTML = breadcrumb;
   }
-  
+
   bindEvents() {
-    this.breadcrumbElement.addEventListener('click', e => this.handleBreadcrumbClick(e));
-    this.appContent.addEventListener('click', e => this.handleContentClick(e));
+    this.breadcrumbElement.addEventListener("click", (e) =>
+      this.handleBreadcrumbClick(e)
+    );
+    this.appContent.addEventListener("click", (e) =>
+      this.handleContentClick(e)
+    );
   }
-  
-  handleBreadcrumbClick(e) {
-    e.preventDefault();
-    const index = e.target.dataset.index;
-    const parts = [null, null, null];
-    const { book, chapter, verse } = this.getURLParams();
-    parts[index] = null;
-    this.updatePath(...parts);
-    if (index == 0) {
-      this.loadHomePage();
-    }
-    else if (index == 1) {
-      this.loadChaptersPage(book);
-    }
-    else if (index == 2) {
-      this.loadVersesPage(book, chapter);
-    }
-    else if (index == 3) {
-      this.loadTranslationPage(book, chapter, verse);
+
+  getTestament(book) {
+    if (this.bibleData.oldtestament[book]) {
+      return "oldtestament";
+    } else if (this.bibleData.newtestament[book]) {
+      return "newtestament";
+    } else {
+      return null;
     }
   }
 
@@ -179,33 +211,70 @@ class BibleNavigator {
     const pathSegments = window.location.pathname.split("/").filter(Boolean); // split by "/" and remove any empty parts
 
     return {
-        book: pathSegments[0] || null,
-        chapter: pathSegments[1] || null,
-        verse: pathSegments[2] || null
+      book: pathSegments[0] || null,
+      chapter: pathSegments[1] || null,
+      verse: pathSegments[2] || null,
     };
   }
-  
+
+  redirectIfInvalid() {
+    const { book, chapter, verse } = this.getURLParams();
+    const testament = this.getTestament(book);
+
+    // Redirect to home page if the book, chapter, or verse does not exist.
+    if (!book) {
+      this.loadHomePage();
+      console.log("book not found");
+    } else if (book && !testament) {
+      this.loadHomePage();
+      console.log("book not found");
+    } else if (book && chapter && !this.bibleData[testament][book][chapter]) {
+      this.loadChaptersPage(book);
+      console.log("chapter not found");
+    } else if (
+      book &&
+      chapter &&
+      verse &&
+      !this.bibleData[testament][book][chapter][verse]
+    ) {
+      this.loadVersesPage(book, chapter);
+      console.log("verse not found");
+    }    
+  }
+
   handleContentClick(e) {
     const { book, chapter, verse } = this.getURLParams();
 
-    if (e.target.classList.contains('book')) {
-        const selectedBook = e.target.dataset.book;
-        this.loadChaptersPage(selectedBook);
-        this.updatePath(selectedBook);
+    if (e.target.classList.contains("book")) {
+      const selectedBook = e.target.dataset.book;
+      this.loadChaptersPage(selectedBook);
+    } else if (e.target.classList.contains("chapter")) {
+      const selectedChapter = e.target.dataset.chapter;
+      this.loadVersesPage(book, selectedChapter);
+    } else if (e.target.classList.contains("verse")) {
+      console.log("verse selected!");
+      const selectedVerse = e.target.dataset.verse;
+      console.log(selectedVerse);
+      this.loadTranslationPage(book, chapter, selectedVerse);
     }
-    else if (e.target.classList.contains('chapter')) {
-        const selectedChapter = e.target.dataset.chapter;
-        this.loadVersesPage(book, selectedChapter);
-        this.updatePath(book, selectedChapter);
-    }
-    else if (e.target.classList.contains('verse')) {
-        console.log('verse selected!');
-        const selectedVerse = e.target.dataset.verse;
-        console.log(selectedVerse);
-        this.loadTranslationPage(book, chapter, selectedVerse);
-        this.updatePath(book, chapter, selectedVerse);
+  }
+
+  handleBreadcrumbClick(e) {
+    e.preventDefault();
+    const index = e.target.dataset.index;
+    const parts = [null, null, null];
+    const { book, chapter, verse } = this.getURLParams();
+
+    parts[index] = null;
+    if (index == 0) {
+      this.loadHomePage();
+    } else if (index == 1) {
+      this.loadChaptersPage(book);
+    } else if (index == 2) {
+      this.loadVersesPage(book, chapter);
+    } else if (index == 3) {
+      this.loadTranslationPage(book, chapter, verse);
     }
   }
 }
 export default BibleNavigator;
-  
